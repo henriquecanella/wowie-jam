5715ab4d3f3e86c47f393ccd57f5c8cf58ccf328
henriquecanella <henrique.mandelli@gmail.com>
2022-08-12 17:57:29 -0300

    add player top-down  movement

--- src/Actors/Player/player.gd
+++ src/Actors/Player/player.gd
@@ -1,10 +1,29 @@
 class_name Player
-extends Sprite
+extends KinematicBody2D
 
+export var speed: int = 100
+var velocity: Vector2 = Vector2.ZERO
 
 func _ready():
 	pass
 
 
-func _process(_delta):
-	pass
+func _physics_process(_delta) -> void:
+	get_input()
+	velocity = move_and_slide(velocity)
+
+
+func get_input() -> void:
+	velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 1
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('ui_down'):
+		velocity.y += 1
+	if Input.is_action_pressed('ui_up'):
+		velocity.y -= 1
+	
+	velocity = velocity.normalized() * speed
+

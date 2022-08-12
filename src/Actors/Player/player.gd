d727746f34d4f9e44517ddf62bc35e042000c8d1
henriquecanella <henrique.mandelli@gmail.com>
2022-08-12 19:50:02 -0300

    separate input and movement logic

--- src/Actors/Player/player.gd
+++ src/Actors/Player/player.gd
@@ -2,28 +2,15 @@ class_name Player
 extends KinematicBody2D
 
 export var speed: int = 100
+
 var velocity: Vector2 = Vector2.ZERO
 
-func _ready():
-	pass
-
+var game_controller = GameController.new()
 
 func _physics_process(_delta) -> void:
-	get_input()
+	move_player()
+
+
+func move_player() -> void:
+	velocity = game_controller.get_movement_input().normalized() * speed
 	velocity = move_and_slide(velocity)
-
-
-func get_input() -> void:
-	velocity = Vector2.ZERO
-	
-	if Input.is_action_pressed('ui_right'):
-		velocity.x += 1
-	if Input.is_action_pressed('ui_left'):
-		velocity.x -= 1
-	if Input.is_action_pressed('ui_down'):
-		velocity.y += 1
-	if Input.is_action_pressed('ui_up'):
-		velocity.y -= 1
-	
-	velocity = velocity.normalized() * speed
-
--- src/Services/game_controller.gd
+++ src/Services/game_controller.gd
@@ -1,1 +1,16 @@
 class_name GameController
+
+
+func get_movement_input() -> Vector2:
+	var input = Vector2()
+
+	if Input.is_action_pressed('ui_right'):
+		input.x +=1
+	if Input.is_action_pressed('ui_left'):
+		input.x -= 1
+	if Input.is_action_pressed('ui_down'):
+		input.y += 1
+	if Input.is_action_pressed('ui_up'):
+		input.y -= 1
+	
+	return input

7b0ca359f0ebbf1e3ab3a1fa00ef3e0ba1e9691a
henriquecanella <henrique.mandelli@gmail.com>
2022-08-12 17:23:09 -0300

    project config details

---
+++ src/Actors/CLEBER/cleber.gd
@@ -0,0 +1,10 @@
+class_name Cleber
+extends Control
+
+
+func _ready():
+	pass
+
+
+func _process(_delta):
+	pass
---
+++ src/Actors/Player/player.gd
@@ -0,0 +1,10 @@
+class_name Player
+extends Sprite
+
+
+func _ready():
+	pass
+
+
+func _process(_delta):
+	pass

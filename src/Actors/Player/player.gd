f05d99e095816eaca334793e35aa64699e684cb2
henriquecanella <henrique.mandelli@gmail.com>
2022-08-14 01:49:38 -0300

    add tilemap

--- src/Actors/Player/player.gd
+++ src/Actors/Player/player.gd
@@ -1,14 +1,18 @@
 class_name Player
 extends KinematicBody2D
 
-export var speed: int = 100
+export var speed: int = 75
 
 var velocity: Vector2 = Vector2.ZERO
 
 var game_controller = GameController.new()
 
+onready var sprite = get_node("Sprite")
+onready var animation = get_node("AnimationPlayer")
+
 func _physics_process(_delta) -> void:
 	move_player()
+	update_sprite()
 
 
 func move_player() -> void:
@@ -16,3 +20,25 @@ func move_player() -> void:
 		velocity = game_controller.get_movement_input().normalized() * speed
 		velocity = move_and_slide(velocity)
 
+
+func update_sprite() -> void:
+	if velocity.y < 0:
+		sprite.texture = load("res://assets/Player/back.png")
+		if self.scale.x > 0:
+			animation.play("move-right-front")
+		else:
+			animation.play("move-left-front")
+	if velocity.y > 0:
+		sprite.texture = load("res://assets/Player/front.png")
+		if self.scale.x > 0:
+			animation.play("move-right-front")
+		else:
+			animation.play("move-left-front")
+
+	if velocity.x > 0:
+		animation.play("move-right-front")
+	if velocity.x < 0:
+		animation.play("move-left-front")
+
+	if Singleton.can_act == false:
+		animation.stop()
